6c54d55012ea1aad15e684a37fd945445cd66d28
TatyanaRyzh <tatyana.ryzhenkova@devexpress.com>
2018-07-02 17:05:44 +0300

    Change version to 0.12.1

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import Header from '../components/Header.js'
+import Header from './Header.js'
 
 const App = () => (
   <div>
     <Helmet>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="stylesheet" href="/bootstrap.min.css" />
-      <title>reSolve Hello World</title>
+      <title>reSolve Wordseeker</title>
     </Helmet>
 
     <Header />
