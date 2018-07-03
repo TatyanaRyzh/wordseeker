344dcc0447cc79e0b754098b5dc0f2d324421406
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-03 15:53:06 +0300

    Add table letters

--- client/components/Board.js
+++ client/components/Board.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Letter from './Letter.js'
+const size = 12;
+let letters = [];
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++){
+        letters.push(<Letter/>);
+    }
+}
 
 const Board = () => (
   <div className="board" style={{width: "100%", float: "left",  height: "73vh"}}>
-    Board
+  {letters}    
   </div>
 )
 
---
+++ client/components/Letter.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Letter = () => (
+  <div className="letter" style={{float: "left", 
+  width: "4vw", 
+  height: "6vh", 
+  fontSize: "28px", 
+  fontFamily: "Calibri", 
+  color: "#3b3b3b", 
+  textAlign: "center",
+  verticalAlign: "middle",
+  lineHeight: "6vh"}}>
+    b
+  </div>
+)
+
+export default Letter
