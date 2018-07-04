b9b8d680e135d4c20a86f6b93a1160e26d25d9a9
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-04 13:02:50 +0300

    Added selection of words

--- client/components/Board.js
+++ client/components/Board.js
@@ -2,19 +2,32 @@ import React from 'react'
 import Letter from './Letter.js'
 
 
-const Board = ({ board }) => {
+const Board = ({ board, points }) => {
     const size = 12;
     let letters = [];
 
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            letters.push(<Letter key={size * i + (j + 1)} value={board[i][j]} />);
+    for (let cell = 0; cell < size; cell++) {
+        for (let row = 0; row < size; row++) {
+            if  (cell === points[0].y && row === points[0].x){
+                letters.push(<Letter key={size * cell + (row + 1)} value={board[cell][row]} points={points}/>);
+            } else {
+                letters.push(<Letter key={size * cell + (row + 1)} value={board[cell][row]}/>);
+            }
         }
     }
 
     return (
         <div className="board">
             {letters}
+            <div className="word" 
+                style={{
+                    width: 4*(points[points.length-1].x - points[0].x + 1) + "vw", 
+                    height: 6*(points[points.length - 1].y - points[0].y + 1) +"vh", 
+                    backgroundColor: "red", 
+                    opacity: "0.2", 
+                    transform: "translate(calc(4vw * " + points[0].x + "), calc(6vh * " + points[0].y + ")"
+                }}>
+            </div>
         </div>
     )
 }
@@ -30,6 +43,7 @@ export default () => {
         }
         arr.push(arr2);
     }
+    let points = [{x:3,y:4},{x:3,y:5},{x:3,y:6},{x:3,y:7}];
 
-    return (<Board board={arr} />)
+    return (<Board board={arr} points={points} />)
 }
--- client/components/Letter.js
+++ client/components/Letter.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const Letter = ({ value }) => (
-    <div className="letter">
-        {value}
-    </div>
-)
+const Letter = ({ value, points }) => {
+  /*  let coverElement = (<div></div>);
+    
+    if (points) {
+        coverElement = (<div style={{width: 4*(points[points.length-1].x - points[0].x + 1) + "vw", height: 6*(points[points.length - 1].y - points[0].y + 1) +"vh", backgroundColor: "red", opacity: "0.2"}}></div>)
+        coverElement = (<div style={{width: "4vw", height: "6vh", backgroundColor: "red", opacity: "0.2"}}></div>)
+    }*/
+
+    return (
+            <div className="letter" style={{color: points ? "blue" : "black"}}>
+                {value}
+            </div>
+    )
+}
 
 export default Letter
