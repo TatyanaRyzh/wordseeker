65e94da313f909bc667aa8339aa15ac61700c952
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-09 16:51:43 +0300

    Diagonal element

--- client/components/Board.js
+++ client/components/Board.js
@@ -21,9 +21,21 @@ function defineForm(points) {
     if (firstX === secondX) {
         position = "vertical";
         direction = firstY < secondY ? "positive" : "negative";
-    } else {
+    } else if (firstY === secondY) {
         position = "horizontal";
         direction = firstX < secondX ? "positive" : "negative";
+    } else if ((firstX < secondX) && (firstY < secondY)) {
+        position = "diagonal1";
+        direction =  "positive";
+    } else if ((firstX > secondX) && (firstY > secondY)) {
+        position = "diagonal1";
+        direction =  "negative";
+    } else if ((firstX > secondX) && (firstY < secondY)) {
+        position = "diagonal2";
+        direction =  "positive";
+    } else if ((firstX < secondX) && (firstY > secondY)) {
+        position = "diagonal2";
+        direction =  "negative";
     }
 
     return [position, direction];
@@ -37,17 +49,25 @@ function selectWord(word) {
     let translateY = form[1] === "positive" ? points[0].y : points[points.length - 1].y;
     let wordLength = LETTER_SIZE * points.length + DIMENTION;
 
-    return (<div className="word"
-        style={{
-            position: "absolute",
-            width: form[0] === "vertical" ? LETTER_SIZE + DIMENTION : wordLength,
-            height: form[0] === "vertical" ? wordLength : LETTER_SIZE + DIMENTION,
-            backgroundColor: word.isMine ? "#3c4da9" : "#bebebe",
-            borderRadius: "30px",
-            opacity: "0.35",
-            transform: "translate(calc(" + LETTER_SIZE + DIMENTION + " * " + translateX + "), calc(" + LETTER_SIZE + DIMENTION + " * " + translateY + "))"
-        }}>
-    </div>);
+    if (form[0] === "diagonal1") {
+        let scaleLength = Math.sqrt(Math.pow(LETTER_SIZE, 2) + Math.pow(LETTER_SIZE, 2)) * points.length;
+        let rotation = 45;
+        if (form[1] === "positive") {
+            return (<div className="word"
+            style={{
+                position: "absolute",
+                width: scaleLength + DIMENTION,
+                height: LETTER_SIZE + DIMENTION,
+                backgroundColor: word.isMine ? "#3c4da9" : "#bebebe",
+                borderRadius: "30px",
+                opacity: "0.35",
+                transformOrigin: "left center",
+                transform: "translate(calc(6.1vh * " + translateX  + "), calc((6.1vh * " + translateY  + ") - " + LETTER_SIZE / 2 + "vh)) rotate(" + rotation + "deg)"
+            }}>
+        </div>);
+        }
+    }
+        
 }
 
 const Board = ({ board, selectedWords }) => {
@@ -80,11 +100,15 @@ const mapStateToProps = state => ({
     board: !!state.viewModels[viewModelName][aggregateId]
       ? state.viewModels[viewModelName][aggregateId].board
       : null,
-      selectedWords:  [
+      /*selectedWords:  [
         { coords: [{ x: 5, y: 6 }, { x: 5, y: 7 }, { x: 5, y: 8 }, { x: 5, y: 9 }], isMine: true },
         { coords: [{ x: 10, y: 4 }, { x: 10, y: 3 }, { x: 10, y: 2 }, { x: 10, y: 1 }], isMine: false },
         { coords: [{ x: 6, y: 1 }, { x: 7, y: 1 }, { x: 8, y: 1 }, { x: 9, y: 1 }, { x: 10, y: 1 }], isMine: true },
         { coords: [{ x: 6, y: 11 }, { x: 5, y: 11 }, { x: 4, y: 11 }, { x: 3, y: 11 }, { x: 2, y: 11 }, { x: 1, y: 11 }], isMine: false }
+    ]*/
+    selectedWords : [
+        { coords: [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 4 }], isMine: true },
+        { coords: [{ x: 4, y: 4 }, { x: 3, y: 5 }, { x: 2, y: 6 }, { x: 1, y: 7 }], isMine: true }
     ]
   })
   
--- client/components/Letter.js
+++ client/components/Letter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Letter = ({ value }) => {
-    return (<div className="letter">{value}</div>)
+    return (<div className="letter" style={{border: "1px solid"}}>{value}</div>)
 }
 
 export default Letter
