0464bde1556422b8ff887650475336d393dc0e2b
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-12 12:13:17 +0300

    Make base selection

--- client/components/Board.js
+++ client/components/Board.js
@@ -15,43 +15,95 @@ const DIMENTION = "vh";
 function defineForm(points) {
     let firstX = points[0].x;
     let firstY = points[0].y;
-    let secondX = points[1].x;
-    let secondY = points[1].y;
 
     let direction = "positive";
     let angle = 90;
 
-    if (firstX === secondX) {
-        direction = firstY < secondY ? "positive" : "negative";
-    } else if (firstY === secondY) {
-        angle = 0;
-        direction = firstX < secondX ? "positive" : "negative";
-    } else if ((firstX < secondX) && (firstY < secondY)) {
-        angle = 45;
-        direction = "positive";
-    } else if ((firstX > secondX) && (firstY > secondY)) {
-        angle = 45;
-        direction = "negative";
-    } else if ((firstX > secondX) && (firstY < secondY)) {
-        angle = 135;
-        direction = "positive";
-    } else if ((firstX < secondX) && (firstY > secondY)) {
-        angle = 135;
-        direction = "negative";
+    if (points.length > 1) {
+
+        let lastX = points[points.length - 1].x;
+        let lastY = points[points.length - 1].y;
+        //Hor +
+        if (lastX > points[points.length - 2].x) { 
+            let length = lastX - points[0].x;
+            points.splice(1,points.length - 2);
+            for (let i = length - 1; i >= 1; i--) {
+                points.splice(1, 0, {x: points[0].x + i, y: points[0].y});
+            }
+        //Ver +
+        } else if (lastY > points[points.length - 2].y) {
+            let length = lastY - points[0].y;
+            points.splice(1,points.length - 2);
+            for (let i = length - 1; i >= 1; i--) {
+                points.splice(1, 0, {x: points[0].x, y: points[0].y + i});
+            }
+        //Hor -
+        } else if (lastX < points[points.length - 2].x) {
+            let length = points[0].x - lastX;
+            points.splice(1,points.length - 2);
+            for (let i = length - 1; i >= 1; i--) {
+                points.splice(1, 0, {x: points[0].x - i, y: points[0].y});
+            }
+        //Ver - 
+        } else if (lastY < points[points.length - 2].y) {
+            let length = points[0].y - lastY;
+            points.splice(1,points.length - 2);
+            for (let i = length - 1; i >= 1; i--) {
+                points.splice(1, 0, {x: points[0].x, y: points[0].y - i});
+            }
+        //Diag 45 +
+        } else {
+            let length = lastY - points[0].y;
+            points.splice(1,points.length - 2);
+            for (let i = length - 1; i >= 1; i--) {
+                points.splice(1, 0, {x: points[0].x + i, y: points[0].y + i});
+            }
+        }
+
+        //console.log(points);
+
+        let secondX = points[1].x;
+        let secondY = points[1].y;
+
+        if (firstX === secondX) {
+            direction = firstY < secondY ? "positive" : "negative";
+        } else if (firstY === secondY) {
+            angle = 0;
+            direction = firstX < secondX ? "positive" : "negative";
+        } else if ((firstX < secondX) && (firstY < secondY)) {
+            angle = 45;
+            direction = "positive";
+        } else if ((firstX > secondX) && (firstY > secondY)) {
+            angle = 45;
+            direction = "negative";
+        } else if ((firstX > secondX) && (firstY < secondY)) {
+            angle = 135;
+            direction = "positive";
+        } else if ((firstX < secondX) && (firstY > secondY)) {
+            angle = 135;
+            direction = "negative";
+        }
     }
+
     return { direction: direction, angle: angle };
 }
 
 function selectWord(word, index) {
     let points = word.coords;
+
+    if (!points.length) {
+        return;
+    }
+
     let form = defineForm(points);
     let angle = form.angle;
     let direction = form.direction;
+
     let length = points.length;
 
     let wordWidth = angle === 0 ? LETTER_SIZE * length : LETTER_SIZE;
     let wordHeight = angle === 0 ? LETTER_SIZE : LETTER_SIZE * length;
-    
+
     let translateX = direction === "positive" ? points[0].x : points[length - 1].x;
     let translateY = direction === "positive" ? points[0].y : points[length - 1].y;
 
@@ -85,28 +137,54 @@ function selectWord(word, index) {
     </div>);
 }
 
-const Board = ({ board, selectedWords }) => {
-    let letters = [];
-    let selectedWordsMarkup = [];
+class Board extends React.Component {
+    constructor(props) {
+        super(props);
 
-    if (!Array.isArray(board)) return null;
-
-    for (let cell = 0; cell < SIZE; cell++) {
-        for (let row = 0; row < SIZE; row++) {
-            letters.push(<Letter key={SIZE * cell + (row + 1)} value={board[cell][row]} />);
+        this.state = {
+            coords: []
         }
     }
 
-    for (let i = 0; i < selectedWords.length; i++) {
-        selectedWordsMarkup.push(selectWord(selectedWords[i], i));
-    }
+    render() {
+        let board = this.props.board;
+        let selectedWords = this.props.selectedWords;
+        let letters = [];
+        let selectedWordsMarkup = [];
+        let index = 0;
 
-    return (
-        <div className="board">
-            {letters}
-            {selectedWordsMarkup}
-        </div>
-    )
+        let addProcessCoord = (coords) => {
+            let array = this.state.coords.slice();
+            array.push(coords);
+            this.setState({ coords: array })
+        }
+
+
+        if (!Array.isArray(board)) return null;
+
+        for (let cell = 0; cell < SIZE; cell++) {
+            for (let row = 0; row < SIZE; row++) {
+                letters.push(<Letter
+                    key={SIZE * cell + (row + 1)}
+                    coords={{ x: row, y: cell }}
+                    value={board[cell][row]}
+                    addFunc={addProcessCoord}
+                />);
+            }
+        }
+
+        for (index; index < selectedWords.length; index++) {
+            selectedWordsMarkup.push(selectWord(selectedWords[index], index));
+        }
+
+        return (
+            <div className="board">
+                {letters}
+                {selectedWordsMarkup}
+                {selectWord({ isMine: true, coords: this.state.coords }, index)}
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => ({
@@ -116,14 +194,14 @@ const mapStateToProps = state => ({
         ? state.viewModels[viewModelName][aggregateId].square
         : null,
     selectedWords: [
-        { coords: [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 }], isMine: true }, //hor pos
+       /* { coords: [{ x: 1, y: 1 }, { x: 2, y: 1 }, { x: 3, y: 1 }, { x: 4, y: 1 }], isMine: false }, //hor pos
         { coords: [{ x: 6, y: 3 }, { x: 5, y: 3 }, { x: 4, y: 2 }, { x: 3, y: 3 }, { x: 2, y: 3 }], isMine: false }, // hor neg
-        { coords: [{ x: 3, y: 7 }, { x: 3, y: 8 }, { x: 3, y: 9 }], isMine: true }, //ver pos
+        { coords: [{ x: 3, y: 7 }, { x: 3, y: 8 }, { x: 3, y: 9 }], isMine: false }, //ver pos
         { coords: [{ x: 9, y: 4 }, { x: 9, y: 3 }, { x: 9, y: 2 }, { x: 9, y: 1 }], isMine: false }, // ver neg
-        { coords: [{ x: 6, y: 5 }, { x: 7, y: 6 }, { x: 8, y: 7 }, {x: 9, y: 8}, {x: 10, y: 9}], isMine: true }, //45 pos
+        { coords: [{ x: 6, y: 5 }, { x: 7, y: 6 }, { x: 8, y: 7 }, { x: 9, y: 8 }, { x: 10, y: 9 }], isMine: false }, //45 pos
         { coords: [{ x: 10, y: 10 }, { x: 9, y: 9 }, { x: 8, y: 8 }], isMine: false }, // 45 neg
-        { coords: [{ x: 6, y: 5 }, { x: 5, y: 6 }, { x: 4, y: 7 }, {x: 3, y: 8}, {x: 2, y: 9}], isMine: true }, //135 pos
-        { coords: [{ x: 2, y: 8 }, { x: 3, y: 7 }, { x: 4, y: 6 }], isMine: false } // 135 neg
+        { coords: [{ x: 6, y: 5 }, { x: 5, y: 6 }, { x: 4, y: 7 }, { x: 3, y: 8 }, { x: 2, y: 9 }], isMine: false }, //135 pos
+        { coords: [{ x: 2, y: 8 }, { x: 3, y: 7 }, { x: 4, y: 6 }], isMine: false } */ // 135 neg
     ]
 })
 
--- client/components/Letter.js
+++ client/components/Letter.js
@@ -1,7 +1,38 @@
 import React from 'react'
 
-const Letter = ({ value }) => {
-    return (<div className="letter">{value}</div>)
+
+/*const Letter = ({ value, coords, addFunc }) => {
+    return (
+        <div className="letter" onClick={(e) => {addFunc(coords)}}>
+            {value}
+        </div>)
+}*/
+let isDown = false;
+const Letter = ({ value, coords, addFunc }) => {
+    
+
+    function down() {
+        console.log("down");
+        isDown = true;
+        addFunc(coords);    
+    }
+
+    function over() {
+        console.log("over", isDown);
+        if (isDown) {
+            addFunc(coords); 
+        }
+    }
+
+    function up() {
+        console.log("up");
+        isDown = false;
+    }
+
+    return (
+        <div className="letter" onMouseDown={(e) => down(e)} onMouseUp={(e) => up(e)} onMouseOver={(e) => over(e)}>
+            {value}
+        </div>)
 }
 
 export default Letter
