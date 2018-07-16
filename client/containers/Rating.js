c74b44485927ad551dae1577cfde0af917fd8c4c
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-16 17:15:51 +0300

    Add rating

--- client/containers/Rating.js
+++ client/containers/Rating.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { connectViewModel } from 'resolve-redux'
 
-const Rating = ({ users}) => (
+const viewModelName = 'rating';
+const aggregateId = 'root';
+
+const Rating = ({ rating }) => (
     <div className="rating">
         Rating
-        {users ? JSON.stringify(users) : null}
+        {rating ? JSON.stringify(rating) : null}
     </div>
 )
 
 const mapStateToProps = state => {
     return {
-        viewModelName: 'users',
-        aggregateId: '*',
-        users: state.viewModels['users']['*']
+        viewModelName,
+        aggregateId,
+        rating: state.viewModels[viewModelName][aggregateId]
     }
 }
 
---
+++ common/view-models/rating.projection.js
@@ -0,0 +1,22 @@
+export default {
+    Init: () => ([]),
+
+    SELECT_WORD: (state, { payload: { userId, word } }) => {
+      const nextState = [...state]
+      let currentUserRating = nextState.find(({ userId: id }) => ( userId === id ))
+      if(!currentUserRating) {
+        nextState.push({ userId, score: word.length })
+      } else {
+        currentUserRating.score += word.length
+      }
+
+      nextState.sort((a, b) => (a.score < b.score))
+
+      for(let i = 0; i< nextState.length; i++) {
+        nextState[i] = { ...nextState[i] }
+      }
+
+      return nextState
+    }
+}
+  
