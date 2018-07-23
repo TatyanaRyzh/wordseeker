6760ef0da8f2b9dfc3ad8ced527a8d0e703674a5
dexforint <dexforint@mail.ru>
2018-07-23 16:17:14 +0300

    congratulations

--- client/containers/Footer.js
+++ client/containers/Footer.js
@@ -9,18 +9,6 @@ const Footer = () =>
         <a href="https://facebook.com/resolvejs/" target="_blank"><img className="small-icon" src="./Facebook.svg" alt="Facebook"></img></a>
         <a href="https://twitter.com/resolvejs/" target="_blank"><img className="small-icon" src="./Twitter_.svg" alt="Twitter"></img></a>
     </div>
-    <div className="fixedoverlay" id="fixedoverlay"></div>
-    <div className="congratulations" id="congratulations">
-        <div className="congratulations-content">
-            <div className="congratulations-title">Congratulations!</div>
-            <img className="reward" src="./reward.svg"></img>
-            <p>Name Name</p>
-            <p>Поздравляем! Вы попали в ТОП10 рейтинга!</p>
-        </div>
-        <div className="congratulations-share-wrapper">
-            <a href="http://twitter.com/share?text=Wordseeker Game&url=" title="Share in Twitter" target="_blank" className="congratulations-share"><img className="congratulations-share-twitter" src="./twitter.svg"></img>Share in Twitter</a>
-        </div>
-    </div>
 </div>
 
 
--- client/containers/Rating.js
+++ client/containers/Rating.js
@@ -18,8 +18,8 @@ function getTopUsers(arr, myId){
     for (let i = 0; i < length; i++){
         let userName = arr[i].username;
         let divClass = "participant";
-        if (i < 3) divClass = "participant topUser";
-        if (arr[i].userId == myId) divClass = "participant me";
+        if (i < 3) divClass += " topUser";
+        if (arr[i].userId == myId) divClass += " me";
         list.push(<div className={divClass} key={i}><div className="place">{(i+1)}</div><div className="avatar">{getLetters(userName)}</div><div className="userName">{userName}</div><div className="userScore">{arr[i].score}</div></div>);
     }
     return list;
@@ -30,11 +30,17 @@ function findMyPlace(rating, myId){
         if (rating[i].userId == myId) return (i + 1);
     }
 }
+let test = "none";
 
+function handleButtonClick() {
+    console.log(test);
+    test = 'none';
+}
 const Rating = (props) => {
     let inRating = false;
     let isFirstTime = props.isFirstTime;
     let ratingAppearing = props.ratingAppearing;
+    
     if (props.rating && props.rating.length) {
         props.rating.forEach((item, index) => {
             if (item.userId === props.userId && index <= 10) {
@@ -49,7 +55,7 @@ const Rating = (props) => {
         }
     }
     if (inRating && ratingAppearing === false) {
-        alert("Congratulation!");
+        test = "block"
         ratingAppearing = props.ratingAppearingUpdate(true).ratingAppearing;
     }
     if (!inRating && ratingAppearing === true) {
@@ -63,8 +69,38 @@ const Rating = (props) => {
                 <div className="myRating">{findMyPlace(props.rating, props.userId)}</div>
             </div>
             {props.rating.length ? getTopUsers(props.rating, props.userId) : null}
-        </div>)
+
+            <div className="fixedoverlay" id="fixedoverlay" style={{display: test}} onClick={handleButtonClick}></div>
+            <div className="congratulations" id="congratulations" style={{display: test}}>
+            <div className="congratulations-content">
+                <div className="congratulations-title">Congratulations!</div>
+                <img className="reward" src="./reward.svg"></img>
+                <p>Name Name</p>
+                <p>Поздравляем! Вы попали в ТОП10 рейтинга!</p>
+            </div>
+            <div className="congratulations-share-wrapper">
+                <a href="http://twitter.com/share?text=Wordseeker Game&url=" title="Share in Twitter" target="_blank" className="congratulations-share"><img className="congratulations-share-twitter" src="./twitter.svg"></img>Share in Twitter</a>
+            </div>
+        </div>
+
+    </div>)
 }
+
+/*
+let fixedoverlay = document.getElementById('fixedoverlay');
+let congratulations = document.getElementById('congratulations');
+
+fixedoverlay .onclick = function(){
+    fixedoverlay.style.display = 'none';
+    congratulations.style.display = 'none';
+}
+
+function congratulate(){
+    fixedoverlay.style.display = 'block';
+    congratulations.style.display = 'block';
+}
+
+*/
 
 const mapStateToProps = state => {
     return {
