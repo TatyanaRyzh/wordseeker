16df3a708857b830b6c6d6025446862b778f692c
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-24 13:49:30 +0300

    Added to the chat input field

--- client/containers/Chat.js
+++ client/containers/Chat.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
-const Chat = () => <div className="chat">Chat</div>
+const Chat = () => (
+    <div className="chat">
+        <div className="chatTitle">Chat</div>
+
+        <div className="enteryField">
+            <input className="inputMessage" type="text" placeholder="Text..."/> 
+            <img className="send" src="./send.svg"></img>  
+        </div>
+    </div>
+)
 
 export default Chat
