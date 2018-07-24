30e34dda8070f0858865c89a5d4da03529135b54
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-24 17:32:47 +0300

    Added the ability to scroll

--- client/containers/Chat.js
+++ client/containers/Chat.js
@@ -4,18 +4,45 @@ const Chat = () => (
     <div className="chat">
         <div className="chatTitle">Chat</div>
 
-        <div className="chatMessage">
-            <div className="chatName">John Melton</div>
-            <div className="chatText">ok
-                <div className="chatTime">18:40</div>
-            </div> 
-        </div>
+        <div className="messages">
+            <div className="chatMessage">
+                <div className="chatName">John Melton</div>
+                <div className="chatText">ok
+                    <div className="chatTime">18:40</div>
+                </div> 
+            </div>
 
-        <div className="chatMessage myMessage">
-            <div className="chatName">John Melton</div>
-            <div className="chatText">ok
-                <div className="chatTime">18:40</div>
-            </div> 
+            <div className="chatMessage myMessage">
+                <div className="chatName">Author</div>
+                <div className="chatText">
+                    The create-resolve-app package is used to create a reSolve-based application. You can use it to get one of the examples, or create an empty application. By default, create-resolve-app generates an empty Hello World application. You can start working on your new reSolve application in just 2 minutes using npx!
+                    <div className="chatTime">18:44</div>
+                </div> 
+            </div>
+
+            <div className="chatMessage myMessage">
+                <div className="chatName">Author</div>
+                <div className="chatText">
+                    The create-resolve-app package is used to create a reSolve-based application. You can use it to get one of the examples, or create an empty application. By default, create-resolve-app generates an empty Hello World application. You can start working on your new reSolve application in just 2 minutes using npx!
+                    <div className="chatTime">18:44</div>
+                </div> 
+            </div>
+
+            <div className="chatMessage myMessage">
+                <div className="chatName">Author</div>
+                <div className="chatText">
+                    The create-resolve-app package is used to create a reSolve-based application. You can use it to get one of the examples, or create an empty application. By default, create-resolve-app generates an empty Hello World application. You can start working on your new reSolve application in just 2 minutes using npx!
+                    <div className="chatTime">18:44</div>
+                </div> 
+            </div>
+
+            <div className="chatMessage myMessage">
+                <div className="chatName">Author</div>
+                <div className="chatText">
+                    The create-resolve-app package is used to create a reSolve-based application. You can use it to get one of the examples, or create an empty application. By default, create-resolve-app generates an empty Hello World application. You can start working on your new reSolve application in just 2 minutes using npx!
+                    <div className="chatTime">18:44</div>
+                </div> 
+            </div>
         </div>
 
         <div className="enteryField">
