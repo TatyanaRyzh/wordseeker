26398418debf8bed20bc66612a7fa12c28cdc256
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-24 16:20:57 +0300

    Add style for me and another message

--- client/containers/Chat.js
+++ client/containers/Chat.js
@@ -4,6 +4,20 @@ const Chat = () => (
     <div className="chat">
         <div className="chatTitle">Chat</div>
 
+        <div className="chatMessage">
+            <div className="chatName">John Melton</div>
+            <div className="chatText">ok
+                <div className="chatTime">18:40</div>
+            </div> 
+        </div>
+
+        <div className="chatMessage myMessage">
+            <div className="chatName">John Melton</div>
+            <div className="chatText">ok
+                <div className="chatTime">18:40</div>
+            </div> 
+        </div>
+
         <div className="enteryField">
             <input className="inputMessage" type="text" placeholder="Text..."/> 
             <img className="send" src="./send.svg"></img>  
