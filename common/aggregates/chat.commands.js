d12afeba1bad039053186a9386a253062e08c4a2
Lykoi18 <aiden.ghost666@gmail.com>
2018-07-27 11:10:07 +0300

    Fix time bug

--- common/aggregates/chat.commands.js
+++ common/aggregates/chat.commands.js
@@ -4,6 +4,9 @@ export default {
     const time = new Date()
     const hours = time.getHours()
     const minutes = time.getMinutes()
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
       return {
           type: 'SEND_MESSAGE',
           payload: {
